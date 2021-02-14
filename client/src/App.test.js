1263813f1d4bef0055d92aad6cd082b8584c8e56
Hagar Livneh <hagar.livneh@axelspringer.com>
2021-02-14 17:24:59 +0100

    Fixed exiting test issue and added checkout to cd part in action

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,5 +2,4 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-afterAll(() => setTimeout(() => process.exit(), 1000));
 test('renders learn react link', () => {});
