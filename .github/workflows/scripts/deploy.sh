7c6cb7d0501103de29f87359cfdff7404aefed77
Hagar Livneh <hagar.livneh@axelspringer.com>
2021-02-14 18:55:35 +0100

    Moved deploy to script

---
+++ .github/workflows/scripts/deploy.sh
@@ -0,0 +1,11 @@
+docker build -t hagli8/multi-client:latest -t hagli8/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t hagli8/multi-server:latest -t hagli8/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t hagli8/multi-worker:latest -t hagli8/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push hagli8/multi-client:latest
+docker push hagli8/multi-server:latest
+docker push hagli8/multi-worker:latest
+
+docker push hagli8/multi-client:$SHA
+docker push hagli8/multi-server:$SHA
+docker push hagli8/multi-worker:$SHA
