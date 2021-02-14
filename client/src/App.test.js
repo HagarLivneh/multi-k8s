0b58de1881ded02035fddca2963cc7072426cc3d
Hagar Livneh <hagar.livneh@axelspringer.com>
2021-02-14 17:03:42 +0100

    Force exit tests step

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,4 +2,5 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+afterAll(() => setTimeout(() => process.exit(), 1000));
 test('renders learn react link', () => {});
